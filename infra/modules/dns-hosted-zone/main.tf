d882dfc090541f7b85dcaa4756cbf4cbc8eb45e4
Lucas Popp <lucaspopp0@gmail.com>
2026-05-22 15:40:47 -0700

    aws provider name

--- infra/modules/dns-hosted-zone/main.tf
+++ infra/modules/dns-hosted-zone/main.tf
@@ -37,7 +37,7 @@ module "acm_cert" {
   validate    = var.validate
 
   providers = {
-    aws.us_east_1 = aws.us_east_1
+    aws = aws.us_east_1
   }
 }
 
@@ -49,7 +49,7 @@ module "api_cert" {
   validate    = false
 
   providers = {
-    aws.us_east_1 = aws.us_east_1
+    aws = aws.us_east_1
   }
 }
 
@@ -61,7 +61,7 @@ module "auth_cert" {
   validate    = false
 
   providers = {
-    aws.us_east_1 = aws.us_east_1
+    aws = aws.us_east_1
   }
 }
 
