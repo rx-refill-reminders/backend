9d47698dab0d67af2a66593eea4ef6a9dd6ba19c
Lucas Popp <lucaspopp0@gmail.com>
2026-05-22 17:09:34 -0700

    simplify

--- infra/modules/api-gateway/domain.tf
+++ infra/modules/api-gateway/domain.tf
@@ -1,3 +1,18 @@
+module "api_cert" {
+  count = var.domain != null ? 1 : 0
+
+  source = "../dns-acm-certificate"
+
+  domain_name = var.domain.hostname
+  zone_id     = var.domain.zone_id
+  validate    = true
+
+  providers = {
+    aws.default   = aws
+    aws.us_east_1 = aws.us_east_1
+  }
+}
+
 resource "aws_apigatewayv2_domain_name" "domain" {
   count = var.domain != null ? 1 : 0
 
