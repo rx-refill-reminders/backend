31bfb390d60147fc20fc0c3ae521935fb00bcdc6
Lucas Popp <lucaspopp0@gmail.com>
2026-05-25 21:05:46 -0700

    fix: Handler startup fix (#29)
    Fix handler startup

--- api/src/cmd/main.go
+++ api/src/cmd/main.go
@@ -1,3 +1,34 @@
-package cmd
+package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/rx-refill-reminders/backend/api/src/internal/handler"
+	"github.com/rx-refill-reminders/backend/api/src/internal/humaserverless"
+	"github.com/rx-refill-reminders/backend/api/src/internal/middleware"
+)
+
+// Run handles an API Gateway request by dispatching to the pre-built API.
+func handleRequest(
+	ctx context.Context,
+	event events.APIGatewayV2HTTPRequest,
+) (events.APIGatewayV2HTTPResponse, error) {
+	h, err := handler.NewHandlerFromEnv()
+
+	if err != nil {
+		return events.APIGatewayV2HTTPResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       fmt.Sprintf("error parsing environment: %v", err.Error()),
+		}, nil
+	}
+
+	return humaserverless.HttpHandler(ctx, h, event)
+}
+
+func main() {
+	lambda.Start(middleware.DefaultChain(handleRequest))
+}
